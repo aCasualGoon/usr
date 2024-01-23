739af6fed7cff89db4931121f2d8b1a1dbf47d5f
goon <acasualgoon@gmail.com>
2024-01-23 01:20:33 +0100

    Update installconfig.sh script to overwrite additional files
    This commit updates the installconfig.sh script to prompt the user to confirm overwriting additional files, including /etc/pacman.conf. It also adds a check to ensure the script is run as root or with sudo.

--- installconfig.sh
+++ installconfig.sh
@@ -3,14 +3,26 @@
 # script to automatically set up ~/.bashrc etc. to point to ~/.config/usr/.bashrc etc.
 # this will remove any previous configurations so make sure to safe those
 
-echo "Continuing will overwrite [~/.bashrc, ~/.bash_profile, ~/.bash_logout] do you wish to continue?"
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Please run this script as root or with sudo."
+    exit 1
+fi
+
+echo "Continuing will overwrite [~/.bashrc, ~/.bash_profile, ~/.bash_logout, /etc/pacman.conf] do you wish to continue?"
 read -r -p "[Y/N] " response
 # exit if not empty and no variation of yes (including partial)
 if [[ ! -z "$response" && ! "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+    echo "Aborting..."
     exit 1
 fi
+
+# get current user directory as first two directories of path (because running as root)
+userdir=/home$(pwd | cut -d'/' -f1,3)
 
 # link dotfiles
 echo "source ~/.config/usr/.bashrc" > ~/.bashrc
 echo "source ~/.config/usr/.bash_profile" > ~/.bash_profile
 echo "~/.config/usr/.bash_logout" > ~/.bash_logout
+
+# link pacman.conf (for current user directory)
+echo "Include = $userdir/.config/usr/pacman.conf" > /etc/pacman.conf
